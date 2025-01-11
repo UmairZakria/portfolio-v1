c5d31ee58676558d7dccce24fe176356954d97d7
UmairZakria <tomrichard872@gmail.com>
2025-01-12 00:29:29 +0500

    V2 Upgraded

---
+++ app/Article/components/example.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const example = () => {
+  return (
+    <div className=''>
+        <div className='text-sm dark:text-gray-400 text-gray-500 px-5'>Next.js, client-side rendering, server-side rendering, Next.js tutorial, improve SEO, web performance, Next.js SSR, Next.js CSR, step-by-step guide</div>
+        <p className='text-sm'>Learn how to convert a Next.js client-side rendered page to a server-side rendered page with this step-by-step guide. Improve your website's SEO, performance, and user experience today!</p>
+        <br />
+        <h1 className='text-xl md:text-3xl'> How to Convert Next.js Client Side Render Page to Server Side Render Page for Better SEO and Performance? To Convert CSR page to SSR page First we need to Know What is Server-Side Rendering.</h1>
+      
+    </div>
+  )
+}
+
+export default example
